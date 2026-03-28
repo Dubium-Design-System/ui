d835fba3cd52b2d096bbef02e797ee7c118801ba
DubiumEkb <dubium-ekb@yandex.ru>
2026-03-29 04:48:03 +0500

    Update

--- docs/storybook-static/assets/Button.stories-BW60Hce2.js
+++
@@ -1,6 +0,0 @@
-import{i as o}from"./Button-DteFcABb.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-Bms2Gypl.js";import"./preload-helper-PPVm8Dsz.js";const{fn:r}=__STORYBOOK_MODULE_TEST__,p={title:"Components/Button",component:o,parameters:{layout:"centered"},tags:["autodocs"],argTypes:{type:{control:{type:"select"},options:["button","submit","reset"]}},args:{onClick:r()}},t={args:{children:"Button",type:"button"}};t.parameters={...t.parameters,docs:{...t.parameters?.docs,source:{originalSource:`{
-  args: {
-    children: "Button",
-    type: "button"
-  }
-}`,...t.parameters?.docs?.source}}};const c=["Primary"];export{t as Primary,c as __namedExportsOrder,p as default};
---
+++ docs/storybook-static/assets/Button.stories-nkpE1D7A.js
@@ -0,0 +1,6 @@
+import{n as e}from"./chunk-BneVvdWh.js";import{n as t,t as n}from"./dist-CLudSDVe.js";var r,i,a,o;e((()=>{n(),{fn:r}=__STORYBOOK_MODULE_TEST__,i={title:`Components/Button`,component:t,parameters:{layout:`centered`},tags:[`autodocs`],argTypes:{type:{control:{type:`select`},options:[`button`,`submit`,`reset`]}},args:{onClick:r()}},a={args:{children:`Button`,type:`button`}},a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    children: "Button",
+    type: "button"
+  }
+}`,...a.parameters?.docs?.source}}},o=[`Primary`]}))();export{a as Primary,o as __namedExportsOrder,i as default};
